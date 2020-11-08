82c5e73a3ea4e48bc68315fc64fde8fe4e98b9a7
Ondřej Povolný <Remogeus@gmail.com>
2020-11-08 18:20:16 +0100

    assigment 2.4 - cv_03b

---
+++ cv_03b/Core/Inc/sct.h
@@ -0,0 +1,16 @@
+/*
+ * sct.h
+ *
+ *  Created on: Nov 8, 2020
+ *      Author: Bc. Ondřej Povolný
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+void sct_led(uint32_t value);
+void sct_init(void);
+void sct_value(uint16_t value);
+
+#endif
+
